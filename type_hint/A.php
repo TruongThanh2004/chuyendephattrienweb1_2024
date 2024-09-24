e4f83f40271dc28d6ce1eb069fe8960fe38852c6
TruongThanh <thanhdoken113@gmail.com>
2024-09-24 10:34:54 +0700

    upload lab2

---
+++ type_hint/A.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('C.php');
+class A extends C{
+    function a1(){
+        echo "This is function a1 from class
+A.";
+    }
+    
+   
+}
+
+?>
---
+++ type_hint/B.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('C.php');
+class B extends C{
+    function b1(){
+        echo "This is function b1 from class
+B.";
+    }
+    
+   
+}
+
+?>
